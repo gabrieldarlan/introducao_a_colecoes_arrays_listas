0e29e60ac5c290e6592abbf0b8ba2c9f9566745a
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-06-15 13:53:35 -0300

    operacoes compostas

--- src/br/com/alura/array/fucoesDeAgregacao.kt
+++ src/br/com/alura/array/fucoesDeAgregacao.kt
@@ -3,7 +3,7 @@ package br.com.alura.array
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-private fun arrayDeBigDecimal() {
+fun arrayDeBigDecimal() {
     val salarios = bigDecimalArrayof("1500.55", "2000.00", "5000.00", "10000.00")
 
     println(salarios.contentToString())
@@ -11,20 +11,46 @@ private fun arrayDeBigDecimal() {
     val aumento = "1.1".toBigDecimal()
     val salariosComAumento: Array<BigDecimal> = salarios
         .map { salario ->
-            if (salario < "5000.00".toBigDecimal()) {
-                salario + "500.00".toBigDecimal()
-            } else {
-                (salario * aumento).setScale(2, RoundingMode.UP)
-            }
+            calculaAumentoRelativo(salario, aumento)
         }.toTypedArray()
+
     println(salariosComAumento.contentToString())
+
+    val gastoInicial = salariosComAumento.somatoria()
+    println("Gasto inicial $gastoInicial")
+
+    val meses = 6.toBigDecimal()
+    val gastoTotal = salariosComAumento.fold(gastoInicial) { acumulador, salario ->
+        acumulador + (salario * meses).setScale(2, RoundingMode.UP)
+    }
+    println(gastoTotal)
+
+    val media = salariosComAumento
+        .sorted()
+        .takeLast(3)
+        .toTypedArray()
+        .media()
+    println("Média dos salários maiores $media")
+
+    val mediaMenoresSalarios = salariosComAumento
+        .sorted()
+        .take(3)
+        .toTypedArray()
+        .media()
+    println("Média dos salários menores: $mediaMenoresSalarios")
+
 }
 
-
-fun bigDecimalArrayof(vararg valores: String): Array<BigDecimal> {
-    return Array<BigDecimal>(valores.size) { i ->
-        valores[i].toBigDecimal()
+private fun calculaAumentoRelativo(
+    salario: BigDecimal,
+    aumento: BigDecimal
+): BigDecimal {
+    return if (salario < "5000.00".toBigDecimal()) {
+        salario + "500".toBigDecimal()
+    } else {
+        (salario * aumento).setScale(2, RoundingMode.UP)
     }
 }
+
 
 
--- src/br/com/alura/array/main.kt
+++ src/br/com/alura/array/main.kt
@@ -1,7 +1,7 @@
 package br.com.alura.array
 
 fun main() {
-
+    arrayDeBigDecimal()
 }
 
 
