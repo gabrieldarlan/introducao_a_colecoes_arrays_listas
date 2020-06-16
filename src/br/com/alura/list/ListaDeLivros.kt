7b412dcbc0d317fb799cfa71b2ec331acea98f61
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-06-16 08:42:50 -0300

    curso completo

--- src/br/com/alura/list/ListaDeLivros.kt
+++ src/br/com/alura/list/ListaDeLivros.kt
@@ -25,9 +25,3 @@ fun main() {
     println(titulos)
 }
 
-fun List<Livro>.imprimeComMarcadores() {
-    val textoFormatado = this.joinToString(separator = "\n") {
-        " - ${it.titulo} de ${it.autor}"
-    }
-    println(" ### Lista de Livros ### \n${textoFormatado}")
-}
--- src/br/com/alura/list/livros.kt
+++ src/br/com/alura/list/livros.kt
@@ -5,7 +5,7 @@ val listaDeLivros = mutableListOf(
         titulo = "Grande Sertão: Veredas",
         autor = "João Guimarães Rosa",
         anoPublicacao = 1956
-    ),  Livro(
+    ), Livro(
         titulo = "Sagarana de João Guimarães Rosa",
         autor = "João Guimarães Rosa",
         anoPublicacao = 1955
@@ -24,6 +24,62 @@ val listaDeLivros = mutableListOf(
         anoPublicacao = 1881
     ),
 
+    Livro(
+        titulo = "Iracema",
+        autor = "José de Alencar",
+        anoPublicacao = 1865,
+        editora = "Editora B"
+    ),
+
+    Livro(
+        titulo = "Vidas Secas",
+        autor = "Graciliano Ramos",
+        anoPublicacao = 1938,
+        editora = "Editora A"
+    ),
+
+    Livro(
+        titulo = "Mayombe",
+        autor = "Pepetela",
+        anoPublicacao = 1979,
+        editora = "Editora B"
+    ),
+
+    Livro(
+        titulo = "O Cortiço",
+        autor = "Aluísio Azevedo",
+        anoPublicacao = 1890,
+        editora = "Editora B"
+    )
+
+)
+val listaDeLivrosComNulos: MutableList<Livro?> = mutableListOf(
+    null,
+    Livro(
+        titulo = "Grande Sertão: Veredas",
+        autor = "João Guimarães Rosa",
+        anoPublicacao = 1956
+    ),
+    null,
+    Livro(
+        titulo = "Sagarana de João Guimarães Rosa",
+        autor = "João Guimarães Rosa",
+        anoPublicacao = 1955
+    ),
+
+    Livro(
+        titulo = "Minha vida de menina",
+        autor = "Helena Morley",
+        anoPublicacao = 1942,
+        editora = "Editora A"
+    ),
+
+    Livro(
+        titulo = "Memórias Póstumas de Brás Cubas",
+        autor = "Machado de Assis",
+        anoPublicacao = 1881
+    ),
+    null,
     Livro(
         titulo = "Iracema",
         autor = "José de Alencar",
