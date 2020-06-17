5c71b3b87fff6ca6535bd6e1ca0452f6894fb57a
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-06-17 10:54:30 -0300

    funcoes relacionadas a collection e map

--- src/br/com/alura/collections/main.kt
+++ src/br/com/alura/collections/main.kt
@@ -1,100 +1,55 @@
 package br.com.alura.collections
 
 fun main() {
-    val pedidos: MutableMap<Int, Double> = mutableMapOf(
-        Pair(1, 20.0),
-        Pair(2, 64.0),
-        3 to 50.0,
-        4 to 100.0,
-        5 to 100.0,
-        6 to 80.0
+    val pedidos = listOf(
+        Pedido(1, 20.0),
+        Pedido(2, 60.0),
+        Pedido(3, 30.0),
+        Pedido(4, 70.0),
+        Pedido(5, 40.0)
+    )
+    println(pedidos)
+    val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido: Pedido ->
+        pedido.numero
+    }
+    println(pedidosMapeados)
+    println(pedidosMapeados[1])
+
+    val pedidosFreteGratis = pedidos.associateWith { pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratis)
+    println(pedidosFreteGratis[Pedido(numero = 2, valor = 60.0)])
+
+//    val mapa: Map<Boolean, Pedido> = pedidos.associateBy { pedido ->
+//        pedido.valor > 50.0
+//    }
+//    println(mapa)
+    val pedidosFretesGratisAgrupados: Map<Boolean, List<Pedido>> = pedidos.groupBy { pedido: Pedido ->
+        pedido.valor > 50
+    }
+    println(pedidosFretesGratisAgrupados)
+    println(pedidosFretesGratisAgrupados[true])
+    val nomes = listOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Ana",
+        "Paulo",
+        "Maria",
+        "Mario",
+        "Gisele"
     )
 
-//    val valorPedido = pedidos.getValue(5)
-//    println(valorPedido)
-    val mensagem: Double? = pedidos.getOrElse(0, {
-        0.0
-    })
-    println(mensagem)
-    println(pedidos.getOrDefault(1, -1.0))
-    println(pedidos.getOrDefault(0, -1.0))
-
-    println(pedidos.keys)
-
-    for (numero in pedidos.keys) {
-        println("Pedido $numero")
+    val agenda = nomes.groupBy { nome ->
+        nome.first()
     }
-
-    println(pedidos.values)
-
-    for (valor in pedidos.values) {
-        println("Valor do pedido: $valor")
-    }
-
-    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
-        numero % 2 == 0 && valor > 70.0
-    }
-    println(pedidosFiltrados)
-
-    val pedidosAcima = pedidos.filterValues { valor ->
-        valor > 70.0
-    }
-
-    println(pedidosAcima)
-
-    val pedidosPares = pedidos.filterKeys { numero ->
-        numero % 2 == 0
-    }
-    println(pedidosPares)
-    println(pedidos + mapOf(7 to 90.0, 8 to 20.0))// monta um novo map
-    println(pedidos)
-
-    println(pedidos - listOf(6, 5))
-    println(pedidos)
-
-//    pedidos.putAll(setOf<Pair<Int, Double>>(7 to 90.0, 8 to 20.0, 8 to 30.0))
-    pedidos += setOf<Pair<Int, Double>>(7 to 90.0, 8 to 20.0, 8 to 30.0)
-    println(pedidos)
-
-    pedidos.keys.remove(1)
-    println(pedidos)
-
-    pedidos.values.remove(50.0)
-    println(pedidos)
-
-    pedidos.values.remove(100.0)
-    println(pedidos)
-
-    pedidos -= 6
-    println(pedidos)
+    println(agenda)
+    println(agenda['A'])
 }
 
-fun testaMap(pedidos: MutableMap<Int, Double>) {
-    println(pedidos)
-    val pedido = pedidos[1]
-    pedido?.let {
-        println("pedido $it")
-    }
-    for (p: Map.Entry<Int, Double> in pedidos) {
-        println("número do pedido: ${p.key}")
-        println("valor do pedido: ${p.value}")
-    }
-    pedidos[4] = 70.0
-    println(pedidos)
-    pedidos.put(5, 70.0)
-    println(pedidos)
-    pedidos[1] = 100.0
-    println(pedidos)
-    pedidos.putIfAbsent(6, 200.0)
-    println(pedidos)
-    pedidos.putIfAbsent(6, 200.0)
-    println(pedidos)
+data class Pedido(val numero: Int, val valor: Double)
 
-    pedidos.remove(6)
-    println(pedidos)
-    pedidos.remove(3, 100.0)
-    println(pedidos)
-}
 
 
 
